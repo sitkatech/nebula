72074d5948b4f1437971e6ca4812956798ec193d
Mack Peters <mack.peters@sitkatech.com>
2021-09-28 10:32:01 -0700

    Diversion scenario fully works with  populating from URL, updating some ng-containers to ngContainer

--- Source/Nebula.Web/src/app/pages/diversion-scenario/diversion-scenario.component.ts
+++ Source/Nebula.Web/src/app/pages/diversion-scenario/diversion-scenario.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { LyraService } from 'src/app/services/lyra.service';
+import { StationSelectCardComponent } from 'src/app/shared/components/station-select-card/station-select-card.component';
 import { UserDetailedDto } from 'src/app/shared/models';
 import { Alert } from 'src/app/shared/models/alert';
 import { AlertContext } from 'src/app/shared/models/enums/alert-context.enum';
@@ -9,6 +11,7 @@ import { CustomRichTextType } from 'src/app/shared/models/enums/custom-rich-text
 import { SiteFilterEnum } from 'src/app/shared/models/enums/site-filter.enum';
 import { HydstraWeatherCondition } from 'src/app/shared/models/hydstra/hydstra-weather-condition';
 import { SiteVariable } from 'src/app/shared/models/site-variable';
+import { AlertService } from 'src/app/shared/services/alert.service';
 
 declare var $: any;
 declare var vegaEmbed: any;
@@ -23,6 +26,7 @@ export class DiversionScenarioComponent implements OnInit {
   public currentUser: UserDetailedDto;
 
   @ViewChild("mapDiv") mapElement: ElementRef;
+  @ViewChild("stationSelect") stationSelect: StationSelectCardComponent;
 
   public mapID: string = 'DiversionScenarioStationSelectMap';
 
@@ -106,28 +110,31 @@ export class DiversionScenarioComponent implements OnInit {
     start_date: new FormControl({ year: this.currentDate.getUTCFullYear(), month: this.currentDate.getUTCMonth() - 2, day: this.currentDate.getUTCDate() }, [Validators.required]),
     end_date: new FormControl({ year: this.currentDate.getUTCFullYear(), month: this.currentDate.getUTCMonth() + 1, day: this.currentDate.getUTCDate() }, [Validators.required]),
     site: new FormControl(null, [Validators.required]),
-    diversionRate: new FormControl(0, [Validators.required]),
-    storageMaxDepth: new FormControl(0, [Validators.required]),
-    storageInitialDepth: new FormControl(0, [Validators.required]),
-    storageArea: new FormControl(0, [Validators.required]),
-    infiltrationRate: new FormControl(0, [Validators.required]),
-    rainfallEventShutdown: new FormControl(true, [Validators.required]),
-    rainfallEventDepthThreshold: new FormControl(0.1, [Validators.required]),
-    eventSeperationTime: new FormControl(6, [Validators.required]),
-    afterRainDelay: new FormControl(72, [Validators.required]),
-    nearestRainfallStation: new FormControl(null, [Validators.required]),
-    monthsActive: new FormControl(this.monthData.map(x => x.id), [Validators.required]),
-    daysActive: new FormControl(this.weekdayData.map(x => x.id), [Validators.required]),
-    hoursActive: new FormControl(this.hourData.map(x => x.id), [Validators.required]),
+    diversion_rate_cfs: new FormControl(0, [Validators.required]),
+    storage_max_depth_ft: new FormControl(0, [Validators.required]),
+    storage_initial_depth_ft: new FormControl(0, [Validators.required]),
+    storage_area_sqft: new FormControl(0, [Validators.required]),
+    infiltration_rate_inhr: new FormControl(0, [Validators.required]),
+    rainfall_event_shutdown: new FormControl(true, [Validators.required]),
+    rainfall_event_depth_threshold: new FormControl(0.1, [Validators.required]),
+    event_seperation_hrs: new FormControl(6, [Validators.required]),
+    after_rain_delay_hrs: new FormControl(72, [Validators.required]),
+    nearest_rainfall_station: new FormControl(null, [Validators.required]),
+    diversion_months_active: new FormControl(this.monthData.map(x => x.id), [Validators.required]),
+    diversion_days_active: new FormControl(this.weekdayData.map(x => x.id), [Validators.required]),
+    diversion_hours_active: new FormControl(this.hourData.map(x => x.id), [Validators.required]),
   });
   public timeSeriesFormDefault = this.timeSeriesForm.value;
   currentlyDisplayingRequestLinkText: string;
+  mapReady: boolean;
 
 
   constructor(
     private cdr: ChangeDetectorRef,
     private lyraService: LyraService,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private alertService: AlertService
   ) {
   }
 
@@ -146,6 +153,10 @@ export class DiversionScenarioComponent implements OnInit {
 
           return 0;
         });
+
+        if (this.mapReady) {
+          this.populateFormFromURL();
+        }
       });
       this.setupFormChangeListener();
     });
@@ -172,19 +183,19 @@ export class DiversionScenarioComponent implements OnInit {
       start_date: this.getDateFromTimeSeriesFormDateObject('start_date'),
       end_date: this.getDateFromTimeSeriesFormDateObject('end_date'),
       site: this.timeSeriesForm.get('site').value,
-      diversion_rate_cfs: this.timeSeriesForm.get('diversionRate').value,
-      storage_max_depth_ft: this.timeSeriesForm.get('storageMaxDepth').value,
-      storage_initial_depth_ft: this.timeSeriesForm.get('storageInitialDepth').value,
-      storage_area_sqft: this.timeSeriesForm.get('storageArea').value,
-      infiltration_rate_inhr: this.timeSeriesForm.get('infiltrationRate').value,
-      rainfall_event_shutdown: this.timeSeriesForm.get('rainfallEventShutdown').value,
-      rainfall_event_depth_threshold: this.timeSeriesForm.get('rainfallEventDepthThreshold').value,
-      event_seperation_hrs: this.timeSeriesForm.get('eventSeperationTime').value,
-      after_rain_delay_hrs: this.timeSeriesForm.get('afterRainDelay').value,
-      nearest_rainfall_station: this.timeSeriesForm.get('nearestRainfallStation').value,
-      diversion_months_active: this.timeSeriesForm.get('monthsActive').value,
-      diversion_days_active: this.timeSeriesForm.get('daysActive').value,
-      diversion_hours_active: this.timeSeriesForm.get('hoursActive').value
+      diversion_rate_cfs: this.timeSeriesForm.get('diversion_rate_cfs').value,
+      storage_max_depth_ft: this.timeSeriesForm.get('storage_max_depth_ft').value,
+      storage_initial_depth_ft: this.timeSeriesForm.get('storage_initial_depth_ft').value,
+      storage_area_sqft: this.timeSeriesForm.get('storage_area_sqft').value,
+      infiltration_rate_inhr: this.timeSeriesForm.get('infiltration_rate_inhr').value,
+      rainfall_event_shutdown: this.timeSeriesForm.get('rainfall_event_shutdown').value,
+      rainfall_event_depth_threshold: this.timeSeriesForm.get('rainfall_event_depth_threshold').value,
+      event_seperation_hrs: this.timeSeriesForm.get('event_seperation_hrs').value,
+      after_rain_delay_hrs: this.timeSeriesForm.get('after_rain_delay_hrs').value,
+      nearest_rainfall_station: this.timeSeriesForm.get('nearest_rainfall_station').value,
+      diversion_months_active: this.timeSeriesForm.get('diversion_months_active').value,
+      diversion_days_active: this.timeSeriesForm.get('diversion_days_active').value,
+      diversion_hours_active: this.timeSeriesForm.get('diversion_hours_active').value
     };
     this.gettingTimeSeriesData = true;
     this.errorOccurred = false;
@@ -291,7 +302,7 @@ export class DiversionScenarioComponent implements OnInit {
     this.selectedVariables = [];
     this.selectedVariables.push(variable);
     this.timeSeriesForm.patchValue({ site: variable.station });
-    this.timeSeriesForm.patchValue({ nearestRainfallStation: variable.nearestRainfallStationInfo.station});
+    this.timeSeriesForm.patchValue({ nearest_rainfall_station: variable.nearestRainfallStationInfo.station});
     this.clearResults();
   }
 
@@ -315,12 +326,9 @@ export class DiversionScenarioComponent implements OnInit {
     }
   }
 
-  // public triggerTimeSeriesWithVariableValuesAndScrollIntoView(el: HTMLElement, variable: SiteVariable) {
-  //   this.scroll(el);
-  //   this.timeSeriesForm.controls.start_date.setValue(this.formatDateForNgbDatepicker(variable.start_date));
-  //   this.timeSeriesForm.controls.end_date.setValue(this.formatDateForNgbDatepicker(variable.end_date));
-  //   this.getTimeSeriesData();
-  // }
+  public scrollIntoView(el: HTMLElement) {
+    this.scroll(el);
+  }
 
   public formatDateForNgbDatepicker(date: Date): any {
     let dateToChange = new Date(date);
@@ -360,4 +368,107 @@ export class DiversionScenarioComponent implements OnInit {
   }
 
   //#endregion
+
+  public setMapReadyToTrueAndCheckIfWeCanPopulateFormFromURL() {
+    this.mapReady = true;
+    if (this.rainfallStations != null) {
+      this.populateFormFromURL();
+    }
+  }
+
+  public populateFormFromURL() {
+    this.route.queryParams.subscribe(params => {
+      if (params == null || params == undefined || !params.hasOwnProperty("json")) {
+        return;
+      }
+
+      let queriedParams;
+
+      try {
+        queriedParams = JSON.parse(params["json"]);
+      }
+      catch (e) {
+        this.alertService.pushAlert(new Alert(`There was an error parsing the URL. ${e}`, AlertContext.Danger, true));
+        return;
+      }
+      
+
+      //Don't bother if we don't have a site
+      if (!queriedParams.hasOwnProperty("site")) {
+        return;
+      }
+
+      let message = this.stationSelect.externalAddSiteVariableReturnMessageIfFailed(queriedParams["site"], "discharge");
+      if (message != null && message != undefined) {
+        this.alertService.pushAlert(new Alert(message, AlertContext.Danger, true));
+        return;
+      }
+
+      if (queriedParams["start_date"] != null) {
+        let start_date = new Date(queriedParams["start_date"]);
+        this.timeSeriesForm.patchValue({start_date : { year: start_date.getUTCFullYear(), month: start_date.getUTCMonth() + 1, day: start_date.getUTCDate() }});
+      }
+
+      if (queriedParams["end_date"] != null) {
+        let end_date = new Date(queriedParams["end_date"]);
+        this.timeSeriesForm.patchValue({end_date : { year: end_date.getUTCFullYear(), month: end_date.getUTCMonth() + 1, day: end_date.getUTCDate() }});
+      }
+
+      let errorMessagesToDisplay = [];
+
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "diversion_rate_cfs", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "storage_max_depth_ft", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "storage_initial_depth_ft", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "storage_area_sqft", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "infiltration_rate_inhr", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "rainfall_event_shutdown", (x => typeof x === "boolean"), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "rainfall_event_depth_threshold", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "event_seperation_hrs", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "after_rain_delay_hrs", (x => typeof x === "number" && !isNaN(x)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(queriedParams, "nearest_rainfall_station", (x => this.rainfallStations.some(y => x == y.properties.station)), errorMessagesToDisplay)        
+      this.updateFormWithValueIfProvidedAndSomePresentPopulateErrorIfNot(queriedParams, "diversion_months_active", this.monthData, ((x, y) => x.some(z => z.id == y)), errorMessagesToDisplay);
+      this.updateFormWithValueIfProvidedAndSomePresentPopulateErrorIfNot(queriedParams, "diversion_days_active", this.weekdayData, ((x, y) => x.some(z => z.id == y)), errorMessagesToDisplay);
+      this.updateFormWithValueIfProvidedAndSomePresentPopulateErrorIfNot(queriedParams, "diversion_hours_active", this.hourData, ((x, y) => x.some(z => z.id == y)), errorMessagesToDisplay);
+
+      this.lyraMessages = errorMessagesToDisplay
+    })
+  }
+
+  public updateFormWithValueIfProvidedAndPresentPopulateErrorIfNot(jsonObject : any, key : string, validityFunction : any, errors : any) {
+    let value = jsonObject[key];
+    let startOfString = jsonObject.hasOwnProperty("site") ? `Station with ID:${jsonObject["site"]}` : "Request";
+    if (value == null || value == undefined) {
+      errors.push(new Alert(`${startOfString} did not provide key:${key}. Will use default.`, AlertContext.Warning, true));
+      return;
+    }
+    
+    if (!validityFunction(value)) {
+      errors.push(new Alert(`${startOfString} provided an invalid value for key:${key}. Will use default. Invalid value was:${value}`, AlertContext.Warning, true));
+      return;
+    }
+
+    this.timeSeriesForm.patchValue({[key] : value});
+  }
+
+  public updateFormWithValueIfProvidedAndSomePresentPopulateErrorIfNot(jsonObject : any, key : string, listToCompare: any, comparisonFunction: any, errors : any) {
+    debugger;
+    let value = jsonObject[key];
+    let startOfString = jsonObject.hasOwnProperty("site") ? `Station with ID:${jsonObject["site"]}` : "Request";
+    if (value == null || value == undefined) {
+      errors.push(new Alert(`${startOfString} did not provide key:${key}. Will use default.`, AlertContext.Warning, true));
+      return;
+    }
+
+    let presentValues = value.filter(x => comparisonFunction(listToCompare, x)).map(x => x);
+    
+    if (presentValues.length != value.length) {
+      if (presentValues.length == 0) {
+        errors.push(new Alert(`${startOfString} provided invalid values for key:${key}. Will use default. Invalid values were:${value.join(", ")}`, AlertContext.Warning, true));
+        return;
+      }
+      errors.push(new Alert(`${startOfString} provided some invalid values for key:${key}. Will use only valid values. Invalid values were:${value.filter(x => !comparisonFunction(listToCompare, x)).map(x => x).join(", ")}`, AlertContext.Warning, true));
+    }
+
+    this.timeSeriesForm.patchValue({[key] : presentValues});
+  }
 }
