70b2634f422e3f5a3d0c79011c15c2d58a588f4b
jamie.quishenberry <jamie.quishenberry@sitkatech.com>
2023-07-12 12:13:51 -0700

    Moving database assets into new database file structure (copied from Splash)

---
+++ Nebula.Database/dbo/Views/dbo.vGeoServerBackbones.sql
@@ -0,0 +1,14 @@
+create View dbo.vGeoServerBackbones
+as
+Select	BackboneSegmentID,
+		CatchIDN,
+		b.BackboneSegmentTypeID,
+		DownstreamBackboneSegmentID,
+		StreamName,
+		BackboneSegmentGeometry4326 as BackboneSegmentGeometry,
+		t.BackboneSegmentTypeName as BackboneSegmentType
+From	dbo.BackboneSegment b 
+join	dbo.BackboneSegmentType t on b.BackboneSegmentTypeID = t.BackboneSegmentTypeID
+
+
+GO
---
+++ Nebula.Database/dbo/Views/dbo.vGeoServerRegionalSubbasins.sql
@@ -0,0 +1,13 @@
+create View dbo.vGeoServerRegionalSubbasins As
+Select
+	RegionalSubbasinID,
+	OCSurveyCatchmentID,
+	OCSurveyDownstreamCatchmentID,
+	DrainID,
+	Watershed,
+	CatchmentGeometry4326 as CatchmentGeometry,
+	CatchmentGeometry.STArea() * 2471054 as Area
+from dbo.RegionalSubbasin
+
+
+GO
